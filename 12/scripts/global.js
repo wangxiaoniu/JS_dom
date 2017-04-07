4610295e1575f073480a20b68e40ee51d80279de
wangxiaoniu <13519870949@163.com>
2017-04-07 11:39:16 +0800

    ss

--- 12/scripts/global.js
+++ 12/scripts/global.js
@@ -1,23 +1,23 @@
 //加载函数
-function addLoadEvent(func){
-	var oldonload=window.onload;
-	if(typeof window.onload !='function'){
-		window.onload=func;
-	}else{
-		window.onload=function (){
-			oldonload();
-			func();
-		}
-	}
+function addLoadEvent(func) {
+  var oldonload = window.onload;
+  if (typeof window.onload != 'function') {
+    window.onload = func;
+  } else {
+    window.onload = function() {
+      oldonload();
+      func();
+    }
+  }
 }
 //在元素之后加元素
-function insertAfter(newElement,targetElement){
-	var parent=targetElement.parentNode;
-	if(parent.lastChild == targetElement){
-		parent.appendChild(newElement);
-	}else{
-		parent.insertAfter(newElement,targetElement.nextSibling);
-	}
+function insertAfter(newElement,targetElement) {
+  var parent = targetElement.parentNode;
+  if (parent.lastChild == targetElement) {
+    parent.appendChild(newElement);
+  } else {
+    parent.insertBefore(newElement,targetElement.nextSibling);
+  }
 }
 //添加样式
 function addClass(element,value){
@@ -49,7 +49,8 @@ function hightlightPage(){
 		}
 	}
 }
-
+//home
+//幻灯片效果
 function moveElement(elementID,final_x,final_y,interval){
 	if(!document.getElementById) return false;
 	if(!document.getElementById(elementID)) return false;
@@ -65,7 +66,7 @@ function moveElement(elementID,final_x,final_y,interval){
 	}
 	var xpos=parseInt(elem.style.left);
 	var ypos=parseInt(elem.style.top);
-	if(xpos == final_x && ypos=final_y){
+	if(xpos == final_x && ypos == final_y){
 		return true;
 	}
 	if(xpos<final_x){
@@ -74,7 +75,7 @@ function moveElement(elementID,final_x,final_y,interval){
 		xpos=xpos +dist;
 	}
 	if(xpos>final_x){
-		var dist=Math.ceil((xpos-fina_x)/10);
+		var dist=Math.ceil((xpos-final_x)/10);
 		xpos= xpos -dist;
 	}
 	if(ypos < final_y){
@@ -87,8 +88,85 @@ function moveElement(elementID,final_x,final_y,interval){
 	}
 	elem.style.left=xpos+"px";
 	elem.style.top=ypos+'px';
-	var repeat="moveElement('"elementID+"',"+final_x+","+final_y+","+interval+")";
+	var repeat="moveElement('"+elementID+"',"+final_x+","+final_y+","+interval+")";
 	elem.movement=setTimeout(repeat,interval);
 }
 
+function prepareSlideshow() {
+  if (!document.getElementsByTagName) return false;
+  if (!document.getElementById) return false;
+  if (!document.getElementById("intro")) return false;
+  var intro = document.getElementById("intro");
+  var slideshow = document.createElement("div");
+  slideshow.setAttribute("id","slideshow");
+  var frame=document.createElement('img');
+  frame.setAttribute('src','images/frame.gif');
+  frame.setAttribute('alt','');
+  frame.setAttribute('id','frame');
+  slideshow.appendChild(frame);
+  var preview = document.createElement("img");
+  preview.setAttribute("src","images/slideshow.gif");
+  preview.setAttribute("alt","a glimpse of what awaits you");
+  preview.setAttribute("id","preview");
+  slideshow.appendChild(preview);
+  insertAfter(slideshow,intro);
+  var links = document.getElementsByTagName("a");
+  for (var i=0; i<links.length; i++) {
+    links[i].onmouseover = function() {
+      var destination = this.getAttribute("href");
+      if (destination.indexOf("index.html") != -1) {
+        moveElement("preview",0,0,5);
+      }
+      if (destination.indexOf("about.html") != -1) {
+        moveElement("preview",-150,0,5);
+      }
+      if (destination.indexOf("photos.html") != -1) {
+        moveElement("preview",-300,0,5);
+      }
+      if (destination.indexOf("live.html") != -1) {
+        moveElement("preview",-450,0,5);
+      }
+      if (destination.indexOf("contact.html") != -1) {
+        moveElement("preview",-600,0,5);
+      }
+    }
+  }
+}
+
+//about
+function showSection(id) {
+  var sections = document.getElementsByTagName("section");
+  for (var i=0; i<sections.length; i++ ) {
+    if (sections[i].getAttribute("id") != id) {
+      sections[i].style.display = "none";
+    } else {
+      sections[i].style.display = "block";
+    }
+  }
+}
+
+
+function prepareInternalnav(){
+	if(!document.getElementsByTagName) return false;
+	if(!document.getElementById) return false;
+	var articles=document.getElementsByTagName('article');
+	if(articles.length==0) return false;
+	var navs =articles[0].getElementsByTagName('nav');
+	if(navs.length ==0) return false;
+	var nav=navs[0];
+	var links=nav.getElementsByTagName('a');
+	for(var i=0;i<links.length;i++){
+		var sectionId=links[i].getAttribute('href').split("#")[1];
+		if(!document.getElementById(sectionId)) continue;
+		document.getElementById(sectionId).style.display="none";
+		links[i].destination=sectionId;
+		links[i].onclick=function(){
+			showSection(this.destination);
+			return false;
+		}
+	}
+
+}
+addLoadEvent(prepareInternalnav);
 addLoadEvent(hightlightPage);
+addLoadEvent(prepareSlideshow);
